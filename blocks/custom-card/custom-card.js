b839cd2970bf8456d83ab34a903ade682ffc1ce2
NiraiRagav <niraivjragavan@gmail.com>
2026-08-20 22:20:38 +0530

    Custom-card

--- blocks/custom-card/custom-card.js
+++ blocks/custom-card/custom-card.js
@@ -1,12 +1,11 @@
 export default function decorate(block) {
-     //  spread operator[...] = Get all the direct children inside the block and store them as an array called cards.
+     //  spread operator[...] ==> all the direct children inside the block
     const cards = [...block.children];
      //  card ==> parameter of the function
     //  cards==> [card1,card2,card3,card4, etc]
     cards.forEach((card) => {              
-        //  ?. ==> optional chaining operator, checks if the card exists before trying to access its classList property.
+        //  ?. ==> optional chaining operator(if condition)
         card?.classList.add('custom_card1');
-        // query each card --> numbers(01,02,03,04)
         const carditem = card.querySelector('div');
         carditem?.classList.add('custom_card1_item');
         const cardimg = card.querySelector('img');
