9cbe4e982d035b7dda0d06e60d6bc0858fd4a229
NiraiRagav <niraivjragavan@gmail.com>
2026-08-20 22:46:23 +0530

    indent fix

--- blocks/custom-card/custom-card.js
+++ blocks/custom-card/custom-card.js
@@ -1,20 +1,20 @@
 export default function decorate(block) {
-     //  spread operator[...] ==> all the direct children inside the block
-    const cards = [...block.children];
-     //  card ==> parameter of the function
-    //  cards==> [card1,card2,card3,card4, etc]
-    cards.forEach((card) => {              
-        //  ?. ==> optional chaining operator(if condition)
-        card?.classList.add('custom_card1');
-        const carditem = card.querySelector('div');
-        carditem?.classList.add('custom_card1_item');
-        const cardimg = card.querySelector('img');
-        cardimg?.classList.add('custom_card1_img');
-        const cardcontent = card.querySelector('p');
-        cardcontent?.classList.add('custom_card1_content');
-        const cardnumber = card.querySelector('h3');
-        cardnumber?.classList.add('custom_card1_number');
-        const cardtitle = card.querySelector('h2');
-        cardtitle?.classList.add('custom_card1_title');
-    });
+  // spread operator[...] ==> all the direct children inside the block
+  const cards = [...block.children];
+  //  card ==> parameter of the function
+  //  cards==> [card1,card2,card3,card4, etc]
+  cards.forEach((card) => {
+    //  ?. ==> optional chaining operator(if condition)
+    card?.classList.add('custom_card1');
+    const carditem = card.querySelector('div');
+    carditem?.classList.add('custom_card1_item');
+    const cardimg = card.querySelector('img');
+    cardimg?.classList.add('custom_card1_img');
+    const cardcontent = card.querySelector('p');
+    cardcontent?.classList.add('custom_card1_content');
+    const cardnumber = card.querySelector('h3');
+    cardnumber?.classList.add('custom_card1_number');
+    const cardtitle = card.querySelector('h2');
+    cardtitle?.classList.add('custom_card1_title');
+  });
 }
