a50f7e413d82a8554a8c1e976a6ea25d80ffb3c1
NiraiRagav <niraivjragavan@gmail.com>
2026-08-21 21:03:59 +0530

    color fix

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,6 +1,5 @@
 export default function decorate(block) {
   const titlecard = block.children(0);
-  console.log(titlecard);
   titlecard.classList.add('titlecard');
   const titlecard1 = titlecard.querySelector('h2');
   console.log(titlecard1);
