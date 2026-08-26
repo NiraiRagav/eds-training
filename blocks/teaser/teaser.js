e77dcf249627f64fcf9e0daff65dbb3004d93a63
NiraiRagav <niraivjragavan@gmail.com>
2026-08-26 18:38:24 +0530

    newcard

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -2,5 +2,5 @@ export default function decorate(block) {
   const titlecard = block.children(0);
   titlecard.classList.add('titlecard');
   const titlecard1 = titlecard.querySelector('h2');
-  console.log(titlecard1);
+  titlecard1.classList.add('titlecard-title');
 }
